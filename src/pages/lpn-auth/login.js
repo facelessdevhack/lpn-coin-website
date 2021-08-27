7addc05b49c00f689ab4ddbb0d603865f3c507a8
facelessdevhack <60548673+facelessdevhack@users.noreply.github.com>
2021-08-28 00:56:31 +0530

    mobile Login Page Update

--- src/pages/lpn-auth/login.js
+++ src/pages/lpn-auth/login.js
@@ -102,33 +102,35 @@ class Login extends React.Component {
                     </form>
                 </TabletView>
                 <MobileOnlyView>
-                    <form onSubmit={this.onSubmit}>
-                        <div id='login-wrapper'>
-                            <div id='overlay'>
-                                <div id='logo-wrapper'>
-                                    <img src={logo} />
-                                </div>
-                                <div id='login-welcometext'>
-                                    <p>Member Login</p>
-                                </div>
-                                <div id='login-input'>
-                                    <p>Email address</p>
-                                    <input value={email} name='email' onChange={this.onChange} id='login-email' placeholder='input your email address' />
-                                    <p>Password</p>
-                                    <input type='password' id='login-email' onChange={this.onChange} value={password} name="password" placeholder='input your password' />
-                                </div>
-                                <div id='login-btn-wrapper'>
-                                    <Button variant="contained" color="primary" value="LOGIN" type="submit">
-                                        Login
-                                    </Button>
-                                    {/* <Link><p>Forgot Password?</p></Link> */}
-                                </div>
-                                {/* <div id='login-create-wrapper'>
-                                    <p>Don't have an account?</p>
-                                    <Link to='/register'><button>Create</button></Link>
-                                </div> */}
-                            </div>
-                        </div>
+                    <form onSubmit={this.onSubmit} style={{backgroundImage: `url(${bg})`}}>
+                        <Container style={{width: '100vw', textAlign: 'center',  height: '100vh', paddingTop: '60px'}}>
+                            <Row>
+                                <Col>
+                                    <div id='logo-wrapper'>
+                                        <img src={logo} />
+                                    </div>
+                                    <div id='login-welcometext-mb'>
+                                        <p>Member Login</p>
+                                    </div>
+                                    <div id='login-input-mb'>
+                                        <p>Email address</p>
+                                        <input value={email} name='email' onChange={this.onChange} id='login-email' placeholder='input your email address' />
+                                        <p>Password</p>
+                                        <input type='password' id='login-email' onChange={this.onChange} value={password} name="password" placeholder='input your password' />
+                                    </div>
+                                    <div id='login-btn-wrapper'>
+                                        <Button variant="contained" color="primary" value="LOGIN" type="submit">
+                                            Login
+                                        </Button>
+                                        {/* <Link><p>Forgot Password?</p></Link> */}
+                                    </div>
+                                    {/* <div id='login-create-wrapper'>
+                                        <p>Don't have an account?</p>
+                                        <Link to='/register'><button>Create</button></Link>
+                                    </div> */}
+                                </Col>
+                            </Row>
+                        </Container>
                     </form>
                 </MobileOnlyView>
             </>
