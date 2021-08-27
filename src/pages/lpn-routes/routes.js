3017dc7501b20281765a35f31da643df7dc11905
facelessdevhack <60548673+facelessdevhack@users.noreply.github.com>
2021-08-28 02:14:22 +0530

    pdf Update

--- src/pages/lpn-routes/routes.js
+++ src/pages/lpn-routes/routes.js
@@ -4,6 +4,7 @@ import login from '../lpn-auth/login';
 import PrivateRoute from '../lpn-common/privateRoute';
 import Home from '../lpn-home/home';
 import UserDashboard from '../lpn-user/dashboard';
+import Whitepaper from '../lpn-whitepaper/whitepaper';
 
 export default function RoutingLPN() {
     return (
@@ -12,6 +13,7 @@ export default function RoutingLPN() {
                 <PrivateRoute exact path='/dashboard' component={UserDashboard} />
                 <Route exact path='/' component={Home} />
                 <Route path='/login' component={login} />
+                <Route path='/whitepaper' component={Whitepaper} />
                 <Route component={Error} />
             </Switch>
         </Router>
---
+++ src/pages/lpn-whitepaper/whitepaper.js
@@ -0,0 +1,254 @@
+import React, { Component } from 'react'
+import amazoOne from '../../assets/amazoImgs/amazoImg_Page_01.jpg'
+import amazotwo from '../../assets/amazoImgs/amazoImg_Page_02.jpg'
+import amazothree from '../../assets/amazoImgs/amazoImg_Page_03.jpg'
+import amazofour from '../../assets/amazoImgs/amazoImg_Page_04.jpg'
+import amazofive from '../../assets/amazoImgs/amazoImg_Page_05.jpg'
+import amazosix from '../../assets/amazoImgs/amazoImg_Page_06.jpg'
+import amazoseven from '../../assets/amazoImgs/amazoImg_Page_07.jpg'
+import amazoeight from '../../assets/amazoImgs/amazoImg_Page_08.jpg'
+import amazonine from '../../assets/amazoImgs/amazoImg_Page_09.jpg'
+import amazoten from '../../assets/amazoImgs/amazoImg_Page_10.jpg'
+import amazoeleven from '../../assets/amazoImgs/amazoImg_Page_11.jpg'
+import amazotwelve from '../../assets/amazoImgs/amazoImg_Page_12.jpg'
+import amazothirteen from '../../assets/amazoImgs/amazoImg_Page_13.jpg'
+import amazofourteen from '../../assets/amazoImgs/amazoImg_Page_14.jpg'
+import amazofifteen from '../../assets/amazoImgs/amazoImg_Page_15.jpg'
+import amazosixteen from '../../assets/amazoImgs/amazoImg_Page_16.jpg'
+import amazoseventeen from '../../assets/amazoImgs/amazoImg_Page_17.jpg'
+import amazoeighteen from '../../assets/amazoImgs/amazoImg_Page_18.jpg'
+import amazonineteen from '../../assets/amazoImgs/amazoImg_Page_19.jpg'
+import amazotwenty from '../../assets/amazoImgs/amazoImg_Page_20.jpg'
+import amazotwentyOne from '../../assets/amazoImgs/amazoImg_Page_21.jpg'
+import amazotwentytwo from '../../assets/amazoImgs/amazoImg_Page_22.jpg'
+import amazotwentythree from '../../assets/amazoImgs/amazoImg_Page_23.jpg'
+import amazotwentyfour from '../../assets/amazoImgs/amazoImg_Page_24.jpg'
+import amazotwentyfive from '../../assets/amazoImgs/amazoImg_Page_25.jpg'
+import amazotwentysix from '../../assets/amazoImgs/amazoImg_Page_26.jpg'
+import amazotwentyseven from '../../assets/amazoImgs/amazoImg_Page_27.jpg'
+import amazotwentyeight from '../../assets/amazoImgs/amazoImg_Page_28.jpg'
+import amazotwentynine from '../../assets/amazoImgs/amazoImg_Page_29.jpg'
+import amazothirty from '../../assets/amazoImgs/amazoImg_Page_30.jpg'
+import { Container, Col, Row } from 'react-bootstrap'
+
+
+export class Whitepaper extends Component {
+    render() {
+        return (
+            <div style={{marginLeft: '-3px'}}>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazoOne} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwo} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazothree} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazofour} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazofive} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazosix} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazoseven} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazoeight} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazonine} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazoten} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazoeleven} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwelve} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazothirteen} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazofourteen} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazofifteen} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazosixteen} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazoseventeen} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazoeighteen} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazonineteen} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwenty} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwentyOne} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwentytwo} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwentythree} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwentyfour} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwentyfive} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwentysix} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwentyseven} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwentyeight} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazotwentynine} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={amazothirty} alt='one' style={{width: '100vw'}}/>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default Whitepaper
